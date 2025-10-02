cb8608b8890f4ab11fdaf555ebf5b82269ac021f
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-10-02 18:54:34 +0000

    .env test

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import spotipy
 from dotenv import load_dotenv
+
 
 # load the .env file variables
 load_dotenv()
 
 
 # Spotify API credentials
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
